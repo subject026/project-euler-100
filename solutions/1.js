d2544621b8508ceaa2f2bbb97d45ef81e2e96dc3
subject026 <sudobuildspeakers@gmail.com>
2020-01-05 16:25:08 +0000

    solved problem 1

--- solutions/1.js
+++ solutions/1.js
@@ -7,9 +7,7 @@ Find the sum of all the multiples of 3 or 5 below the provided parameter value n
 function multiplesOf3and5(number) {
   let sum = 0;
   for (let i = 0; i < number; i++) {
-    if (i % 3 === 0 && i % 5 === 0) sum += i;
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
   }
   return sum;
 }
-
-multiplesOf3and5(1000);
